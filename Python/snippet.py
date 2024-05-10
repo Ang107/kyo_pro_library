ef8192561deb64850ffcfcb629911297594a9f2c
Ang107 <kosukenabeta1007@gmail.com>
2024-05-11 02:08:20 +0900

    change

--- Python/snippet.py
+++ Python/snippet.py
@@ -1,29 +1,29 @@
 import sys
 from collections import deque, defaultdict
 from itertools import (
-    accumulate, #累積和
-    product,   #bit全探索 product(range(2),repeat=n) 
-    permutations,   #permutations : 順列全探索
-    combinations,   #組み合わせ（重複無し） 
-    combinations_with_replacement,  #組み合わせ（重複可）
+    accumulate,  # 累積和
+    product,  # bit全探索 product(range(2),repeat=n)
+    permutations,  # permutations : 順列全探索
+    combinations,  # 組み合わせ（重複無し）
+    combinations_with_replacement,  # 組み合わせ（重複可）
 )
 import math
 from bisect import bisect_left, bisect_right
 from heapq import heapify, heappop, heappush
 import string
 
-#外部ライブラリ
+# 外部ライブラリ
 # from sortedcontainers import SortedSet, SortedList, SortedDict
 
 sys.setrecursionlimit(10**7)
-alph_s = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-alph_l = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+alph_s = tuple(string.ascii_lowercase)
+alph_l = tuple(string.ascii_uppercase)
 around4 = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 上下左右
-around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)) #周囲8マス
-inf = float("inf")  
+around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+inf = float("inf")
 mod = 998244353
-input = lambda: sys.stdin.readline().rstrip()   
-pritn = lambda *x: print(*x)  
+input = lambda: sys.stdin.readline().rstrip()
+pritn = lambda *x: print(*x)
 PY = lambda: print("Yes")
 PN = lambda: print("No")
 SI = lambda: input()
