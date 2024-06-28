972e4c66a8a8fd77cffabff614c72e05ac7332e9
Ang107 <kosukenabeta1007@gmail.com>
2024-06-28 17:59:23 +0900

    rolling hash追加、デバッグ関数削除

--- Python/math.py
+++ Python/math.py
@@ -1,14 +1,3 @@
-# n以下の素数のリストを取得
-def get_Sosuu(n):
-    A = list(range(2, n + 1))
-    p = list()
-    while A[0] ** 2 <= n:
-        tmp = A[0]
-        p.append(tmp)
-        A = [e for e in A if e % tmp != 0]
-    return p + A
-
-
 def get_primes(n):
     # n以下のすべての数について素数かどうかを記録する配列
     is_prime = [True] * (n + 1)
